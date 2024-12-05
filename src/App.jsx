c88fdb8c3244bd7ab332488207bf6b8f2127be7b
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2024-12-05 16:11:56 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+//Here you can import any page
+import HomePage from '../src/paginas/homepage.jsx'
+// import Contact from '../src/paginas/contacto.jsx'
+
+
+
+
+function App() {
+
+
+  return (
+   
+   //Any extra page you guys want to add, just use the Router-Router-Route Components. 
+   <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />    
+        {/* <Route path="/contactos" element={<Contacto />} />        */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/paginas/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../componentes/navbar';
+const Homepage = () =>  {
+  return (
+<div>  
+  <h1>homepage</h1>
+  
+    <Navbar />
+    </div>
+  )
+}
+
+export default Homepage;
+
