57853b27138154bead8270b2fff1acd2dbff43a9
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2024-12-06 11:16:12 -0500

    Instalacion de Tailwing en la app

--- src/paginas/homepage.jsx
+++ src/paginas/homepage.jsx
@@ -4,6 +4,9 @@ const Homepage = () =>  {
   return (
 <div>  
   <h1>homepage</h1>
+  <div class="bg-blue-500 text-white p-4 text-xl">
+  Hola, Tailwind CSS Funciona!
+</div>
   
     <Navbar />
     </div>
